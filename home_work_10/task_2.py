e05f90d82a7088ab37893563d000edcdb32c4023
Yevhenii <barashyk93@gmail.com>
2026-04-20 16:32:21 +0200

    Home work

---
+++ home_work_10/task_2.py
@@ -0,0 +1,11 @@
+def first_word(text):
+    return text.replace('.', ' ').replace(',', ' ').strip().split()[0]
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
